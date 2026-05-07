909a3870406c676ba81b90e91ca64c72fef676b7
Payal120324 <146610738+Payal120324@users.noreply.github.com>
2026-05-07 15:54:24 +0530

    Update utils.py

--- inference/utils.py
+++ inference/utils.py
@@ -1,4 +1,19 @@
 import numpy as np
 
+
 def format_input(data):
-    return np.array(data)
+    return np.array(data, dtype=np.float32)
+
+
+def one_hot_encode(y, num_classes):
+    return np.eye(num_classes)[y]
+
+
+def accuracy(y_true, y_pred):
+    predictions = np.argmax(y_pred, axis=1)
+    return np.mean(predictions == y_true)
+
+
+def shuffle_data(X, y):
+    indices = np.random.permutation(len(X))
+    return X[indices], y[indices]
